28c806d901093dea0f8bbfba4c92edf75ef8c72f
Maik Emanoel <maikemanoel11@gmail.com>
2023-08-25 17:07:20 -0300

    turn Form into component

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ export function App() {
     <>
       <img src={imgCover} alt="" className='w-full h-[185px] absolute top-0 md:object-cover md:object-left-top' />
 
-      <main className='z-10 relative'>
+      <main className='z-10 relative mx-auto max-w-[720px] my-20'>
         <header>
-          <h1>Lista de Compras</h1>
+          <h1 className='text-gray-100 text-2xl font-bold tracking-[0.72px] mb-8'>Lista de Compras</h1>
           <Form />
         </header>
       </main>
