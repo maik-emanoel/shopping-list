1deaa7eee20c3b0b15c796f0e40596e54bce4048
Maik Emanoel <maikemanoel11@gmail.com>
2023-08-26 14:46:01 -0300

    style option & add Label component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,6 +2,8 @@ import { ChevronDown, Plus } from "lucide-react";
 import Select, { SingleValue, components } from "react-select";
 import { onlyNumbers } from "../utils/onlyNumbers";
 import { useState } from "react";
+import { touchIsSupported } from "../utils/touchUtil";
+import { Label } from "./Label";
 
 export function Form() {
   const [isSelectOpen, setIsSelectOpen] = useState(false)
@@ -30,12 +32,12 @@ export function Form() {
   return (
     <form className="text-white flex items-center">
       <div>
-        <label htmlFor="itemInput">Item</label>
+        <Label name="Item" htmlFor="itemInput" />
         <input type="text" id="itemInput" />
       </div>
 
       <div className="">
-        <label htmlFor="quantity">Quantidade</label>
+        <Label name="Quantidade" htmlFor="quantity" />
 
         <div className="flex rounded-md h-10">
           <input
@@ -54,7 +56,9 @@ export function Form() {
             classNames={{
               control: () => "bg-gray-400 text-gray-200 flex items-center justify-between w-[72px] h-full px-3 rounded-r-md text-xs uppercase",
               container: () => "cursor-pointer",
-              dropdownIndicator: () => `transition-all duration-300 ${isSelectOpen ? "rotate-180" : "rotate-0"}`
+              dropdownIndicator: () => `${isSelectOpen ? "rotate-180" : "rotate-0"}`,
+              option: () => `p-3 bg-gray-500 text-sm tracking-[0.42px] ${touchIsSupported ? '' : 'hover:bg-gray-300'}`,
+              menuList: () => "divide-y-[1px] divide-gray-300 border border-gray-300 rounded-md mt-1"
             }}
             defaultValue={optionsQuantity[0]}
             onMenuOpen={() => setIsSelectOpen(true)}
@@ -64,7 +68,7 @@ export function Form() {
       </div>
 
       <div>
-        <label htmlFor="category">Categoria</label>
+        <Label name="Categoria" htmlFor="category" isCategory/>
 
         <div>
           <select name="" className="bg-transparent">
@@ -77,7 +81,7 @@ export function Form() {
         </div>
       </div>
 
-      <button>
+      <button className="w-10 h-10 grid place-items-center bg-purpleNormal rounded-full">
         <Plus />
       </button>
     </form>
