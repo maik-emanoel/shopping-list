1191cb17e16e0bc9f3fa7539060367f83aada57c
Maik Emanoel <maikemanoel11@gmail.com>
2023-08-28 14:02:29 -0300

    create function & variables to handle with items creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,86 @@
-import imgCover from './assets/img-cover.png'
-import { Form } from './components/Form';
+import { Key, useRef, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import imgCover from "./assets/img-cover.png";
+import { Form } from "./components/Form";
+import { Item } from "./components/Item";
+
+interface ItemsArrayProps {
+  id: Key | null | undefined;
+  name: string,
+  quantity: number,
+  typeOfQuantity: string,
+  category: string
+}
 
 export function App() {
+  const [itemName, setItemName] = useState("");
+  const [itemQuantity, setItemQuantity] = useState(1);
+  const [itemTypeOfQuantity, setItemTypeOfQuantity] = useState("unidade");
+  const [itemCategory, setItemCategory] = useState("");
+
+  const [items, setItems] = useState<ItemsArrayProps[]>([]);
+  const selectRef = useRef(null)
+
+  function handleCreateItem() {
+    if (itemName === "" || itemQuantity < 1 || itemCategory === "") {
+      alert('Preencha todos os campos, por favor!')
+      return;
+    }
+
+    const newItem = {
+      id: uuidv4(),
+      name: itemName,
+      quantity: itemQuantity,
+      typeOfQuantity: itemTypeOfQuantity,
+      category: itemCategory,
+    };
+
+    setItems([...items, newItem]);
+    setItemName("")
+    setItemQuantity(1)
+    setItemTypeOfQuantity("unidade")
+    setItemCategory("")
+
+    selectRef.current.clearValue()
+  }
+
   return (
     <>
-      <img src={imgCover} alt="" className='w-full h-[185px] absolute top-0 md:object-cover md:object-left-top' />
+      <img
+        src={imgCover}
+        alt=""
+        className="w-full h-[185px] absolute top-0 md:object-cover md:object-left-top"
+      />
 
-      <main className='z-10 relative mx-auto max-w-[720px] my-20 w-[90%]'>
+      <main className="z-10 relative mx-auto max-w-[720px] my-20 w-[90%]">
         <header>
-          <h1 className='text-gray-100 text-2xl font-bold tracking-[0.72px] mb-8'>Lista de Compras</h1>
-          <Form />
+          <h1 className="text-gray-100 text-2xl font-bold tracking-[0.72px] mb-8">
+            Lista de Compras
+          </h1>
+          <Form
+            setItemName={setItemName}
+            setItemQuantity={setItemQuantity}
+            setItemTypeOfQuantity={setItemTypeOfQuantity}
+            setItemCategory={setItemCategory}
+            itemName={itemName}
+            itemQuantity={itemQuantity}
+            handleCreateItem={handleCreateItem}
+            selectRef={selectRef}
+          />
         </header>
+
+        <div className="mt-8 flex flex-col gap-3">
+          {items.map((item) => (
+            <Item
+              key={item.id}
+              itemName={item.name}
+              itemQuantity={item.quantity}
+              itemTypeOfQuantity={item.typeOfQuantity}
+              itemCategory={item.category}
+            />
+          ))}
+        </div>
       </main>
     </>
   );
