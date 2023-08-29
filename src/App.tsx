341c7c263bbf3c1110281eff50a0f4b6dc2c8d24
Maik Emanoel <maikemanoel11@gmail.com>
2023-08-29 09:06:21 -0300

    add max-width on empty list message

--- src/App.tsx
+++ src/App.tsx
@@ -72,7 +72,7 @@ export function App() {
 
         <div className="mt-8 flex flex-col gap-3">
           {items.length === 0 && (
-            <p className="text-gray-200 text-center mx-auto text-sm w-[80%]">
+            <p className="text-gray-200 text-center mx-auto text-sm w-[80%] max-w-[400px]">
               Você ainda não adicionou nenhum item à sua lista de compras.
               Que tal adicionar agora?
             </p>
